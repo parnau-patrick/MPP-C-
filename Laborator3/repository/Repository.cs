a0e6eae979fa9f509a94d7962ea816de88a7bfe9
Parnau Patrick <parnau.patrick@yahoo.com>
2025-03-12 16:50:02 +0200

    Laborator 3 home.

--- Laborator3/repository/Repository.cs
+++ Laborator3/repository/Repository.cs
@@ -1,6 +1,11 @@
-﻿namespace Laborator3.repository;
+﻿
+namespace Laborator3.Repository ;
 
-public interface Repository
+public interface IRepository<ID, E>
 {
-    
+    E FindOne(ID id);
+    IEnumerable<E> FindAll();
+    E Save(E entity);
+    E Update(E entity);
+    E Delete(ID id);
 }
